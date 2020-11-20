e4b824a12c2075be2a130fa6ce29d0a9878a5196
Megan Smith <megan@tokaypress.com>
2020-11-20 11:42:15 -0800

    fix error

--- components/recipe-card.js
+++ components/recipe-card.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import Link from 'next/link';
 import styles from './recipe-card.module.css';
 // import Heart from './heart';
 
@@ -11,13 +10,11 @@ export default function RecipeCard({
     imageUrl,
 }) {
     return (
-        <Link href={`/recipes/${slug}`}>
-            <a className={styles.card}>
-                <img src={imageUrl} alt={name} className={styles.image} />
-                <h4 className={classNames('headingLg', styles.title)}>{name}</h4>
-                {/* <Heart liked={liked} /> */}
-            </a>
-        </Link>
+        <a href={`/recipes/${slug}`} className={styles.card}>
+            <img src={imageUrl} alt={name} className={styles.image} />
+            <h4 className={classNames('headingLg', styles.title)}>{name}</h4>
+            {/* <Heart liked={liked} /> */}
+        </a>
     );
 }
 
